3839024ed26c8e87cd95b5b79e8428c6e0178dee
Vatthanachai W <barry.cm@traditionasia.com>
2015-09-04 11:42:15 +0700

    commit for update table mapping

--- PlayFramework/app/controllers/Application.scala
+++ PlayFramework/app/controllers/Application.scala
@@ -1,16 +1,40 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.Future
+
+case class Member(code: String, name: String, phone: List[String], email: List[String])
 
 object Application extends Controller {
 
+  var memberForm = Form(
+    tuple(
+      "txtMemberId" -> text,
+      "txtMemberName" -> text,
+      "phone" -> list(text),
+      "email" -> list(text)
+    )
+  )
+
   def index = Action {
-    Ok(views.html.Index("Your new application is ready."))
+    implicit request =>
+      Ok(views.html.Index("Your new application is ready."))
   }
 
-  def save = Action{
+  def save = Action.async {
     implicit request =>
-      Ok(request.body)
+      memberForm.bindFromRequest.fold(
+        formWithErrors => {
+          Future(Redirect(routes.Application.index()).flashing("messageError" -> s"Error mapping form control :- $formWithErrors"))
+        }, memberForm => {
+          val (txtMemberId, txtMemberName, phone, email) = memberForm
+          val member = Member(txtMemberId, txtMemberName, phone, email)
+          val msg = s"code : ${member.code}, name: ${member.name}, phone: ${member.phone}, email: ${member.email}"
+          Future(Ok(msg))
+        }
+      )
   }
 }
